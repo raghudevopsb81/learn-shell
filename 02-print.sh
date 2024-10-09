aebd0e0f1222a5cd03b5fc7ed1d47a9f13d4f16c
SVR Kalluru <raghuk.vit@gmail.com>
2024-10-09 08:15:31 +0530

    echo command

--- 02-print.sh
+++ 02-print.sh
@@ -5,3 +5,24 @@
 
 echo Hello World
 
+# print colors
+# syntax: echo -e "\e[COLmHello World\e[0m"
+# echo -e, -e - enable colors
+# \e[COLm - Tell which color
+# \e[0m - Disable color
+
+# COL       - CODE
+# RED       - 31
+# GREEN     - 32
+# YELLOW    - 33
+# BLUE      - 34
+# MAGENTA   - 35
+# CYAN      - 36
+
+echo -e "\e[31mHello World in Red Color\e[0m"
+echo -e "\e[32mHello World in Green Color\e[0m"
+echo -e "\e[33mHello World in Yellow Color\e[0m"
+echo -e "\e[34mHello World in Blue Color\e[0m"
+echo -e "\e[35mHello World in Magenta Color\e[0m"
+echo -e "\e[36mHello World in Cyan Color\e[0m"
+
