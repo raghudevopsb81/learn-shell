48eba48137153ace5d010f22fdcb6014864b25d5
SVR Kalluru <raghuk.vit@gmail.com>
2024-10-09 08:25:50 +0530

    Adding print statements

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+# If we assign a name to a set of data is called as variable
+# variable=data
+
+# To access variable we use $variable or ${variable}
+
+trainer=John
+
+echo DevOps Trainer - $trainer
+echo AWS Trainer - $trainer
+
+
