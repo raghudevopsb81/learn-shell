9e3408c7d88d63a00669ea8378c7d6ff772f6cb5
SVR Kalluru <raghuk.vit@gmail.com>
2024-10-09 08:37:58 +0530

    Adding print statements

--- 03-variables.sh
+++ 03-variables.sh
@@ -9,3 +9,18 @@ echo DevOps Trainer - $trainer
 echo AWS Trainer - $trainer
 
 
+# Variable name can comprise - a-z, A-Z, 0-9, _
+# OS Background - ALL CAPS - VARIABLE_NAME
+# Java Background - camelCase - variableName
+# Python Background - snake_case - variable_name
+
+# Data - Free to give anything, No data type, Everything is string.
+# In case if we have special characeters in data, then do use double quotes
+# trainer="John Jacob"
+
+
+# src=x.txt
+# dest=y.txt
+# cp $src $dest
+
+
