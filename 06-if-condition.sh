9ef8b8ec637668e272738295dfd482c744bc447a
SVR Kalluru <raghuk.vit@gmail.com>
2024-10-10 08:21:01 +0530

    Adding print statements

---
+++ 06-if-condition.sh
@@ -0,0 +1,39 @@
+# If found in 3 forms
+# 1. Simple if
+
+#if [ expression ]; then
+#  commands
+#fi
+
+# 2. If Else
+
+#if [ expression ]; then
+#  commands-T
+#else
+#  commands-F
+#fi
+
+# 3. Else if
+
+#if [ expression-a ]; then
+#  commands-a
+#elif [ expression-b ]; then
+#  commands-b
+#else
+#  commands
+#fi
+
+
+# Expressions
+# Strings
+# [ abc == bac ]
+# [ $a == $x ]
+# Operators : == , !=, -z
+
+# Numbers
+# [ 1 -eq 2 ]
+# -eq , -ne , -gt , -ge , -le, -le
+
+
+# Files
+# Operators: https://www.man7.org/linux/man-pages/man1/bash.1.html
