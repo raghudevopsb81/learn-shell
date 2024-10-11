953f2223ca63260ec776a8a9570e7533bdc0e3b9
Raghu K <raghuk.vit@gmail.com>
2024-10-11 08:18:16 +0530

    Handling Passwords from Input

--- 02-print.sh
+++ 02-print.sh
@@ -26,3 +26,5 @@ echo -e "\e[34mHello World in Blue Color\e[0m"
 echo -e "\e[35mHello World in Magenta Color\e[0m"
 echo -e "\e[36mHello World in Cyan Color\e[0m"
 
+# We can print new lines by using \n
+echo -e "Hello\nWorld"
